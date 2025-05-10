6cfb66ed18d0bc2fde0576ac912b887fcd8f128c
ThatsHai <nguyendonghaiwork94@gmail.com>
2025-05-10 15:09:42 +0700

    Initialize backend

---
+++ Formatter_Backend/thesis_formatter/thesis_formatter/src/main/java/com/thesis_formatter/thesis_formatter/ThesisFormatterApplication.java
@@ -0,0 +1,13 @@
+package com.thesis_formatter.thesis_formatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesisFormatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThesisFormatterApplication.class, args);
+	}
+
+}
---
+++ Formatter_Backend/thesis_formatter/thesis_formatter/src/test/java/com/thesis_formatter/thesis_formatter/ThesisFormatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thesis_formatter.thesis_formatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesisFormatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
